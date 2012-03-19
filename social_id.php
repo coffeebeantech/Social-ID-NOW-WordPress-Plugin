1e1bf1fc9f13c42a70eafb3e0e66ffb1d7992c17
Guilherme Carreiro <karreiro@gmail.com>
2012-03-19 09:53:48 -0300

    New parameters

--- social_id.php
+++ social_id.php
@@ -11,7 +11,7 @@ class SocialID {
 
   // params - id, size
   function get_social_id( $atts ) {
-    $size = $atts['size'] ? $atts['size'] : 'large';  
+    $size = $atts['size'] ? $atts['size'] : 'large';
 
     return !$atts['id'] ? "" : "<script src='http://www.socialidnow.com/javascripts/badge.js' type='text/javascript'></script>
                                 <div class='social-id-badge' id='sid_badge_".$atts['id']."_".$atts['size']."' style='display: inline-block; margin: 10px'>
@@ -25,19 +25,23 @@ class SocialID {
   }
   function social_id( $atts ) { echo get_social_id( $atts ); }
 
-  // params - group_url, size, width, height 
+  // params - group_url, size, header, join_button, join_button_position, pictures, width, height
   function get_social_id_group( $atts ) {
-    $default_styles = array(
-      'small'  => array('width' => 350, 'height' => 200),
-      'normal' => array('width' => 560, 'height' => 430),
-      'large'  => array('width' => 950, 'height' => 600)
-    );
-    $width  = $atts['width']  ? $atts['width']  : $default_styles[$atts['size']]['width'];
-    $height = $atts['height'] ? $atts['height'] : $default_styles[$atts['size']]['height'];
-    $size = $atts['size'] ? $atts['size'] : 'small';
-    $join = $atts['join_button'] ? $atts['join_button'] : 'yes';
+    $default_atts = array('header'               => 'yes',
+                          'join_button'          => 'yes',
+                          'join_button_position' => 'middle',
+                          'pictures'             => 'yes',
+                          'size'                 => 'small');
+    $default_sizes = array('small'  => array('width' => 350, 'height' => 200),
+                           'normal' => array('width' => 580, 'height' => 430),
+                           'large'  => array('width' => 950, 'height' => 600));
 
-    return !$atts['group_url'] ? "" : "<div class='group-badge' id='group_badge_".$atts['group_url']."_".$size."'style='display: inline-block; margin: 10px'>
+    foreach ($default_atts as $k => $v)
+      if (!$atts[$k]) $atts[$k] = $v;
+    if (!$atts['width'])  $atts['width']  = $default_sizes[$atts['size']]['width'];
+    if (!$atts['height']) $atts['height'] = $default_sizes[$atts['size']]['height'];
+
+    return !$atts['group_url'] ? "" : "<div class='group-badge' id='group_badge_".$atts['group_url']."_".$atts['size']."'style='display: inline-block; margin: 10px'>
                                           <p></p>
                                         </div>
                                         <script>
@@ -47,16 +51,17 @@ class SocialID {
                                               i.scrolling = 'auto';
                                               i.setAttribute('frameBorder', '0');
                                               i.allowTransparency = 'true';
-                                              var iframe = d.getElementById('group_badge_".$atts['group_url']."_".$size."').appendChild(i),
+                                              var iframe = d.getElementById('group_badge_".$atts['group_url']."_".$atts['size']."').appendChild(i),
                                                   doc = iframe.contentWindow.document;
-                                              iframe.style.cssText = 'width: ".$width."px; height: ".$height."px';
-                                              doc.open().write('<body onload=document.location=\"http://www.socialidnow.com/groups/".$atts['group_url']."/badges/".$size."?join_button=".$join."\">');
+                                              iframe.style.cssText = 'width: ".$atts['width']."px; height: ".$atts['height']."px';
+                                              doc.open().write('<body onload=document.location=\"http://www.socialidnow.com/groups/".$atts['group_url']."/badges/".$atts['size']."?join_button=".$atts['join_button']."&join_button_position=".$atts['join_button_position']."&pictures=".$atts['pictures']."&header=".$atts['header']."\">');
                                               doc.close();
                                             })(document);
                                           //]]>
                                         </script>";
   }
   function social_id_group( $atts ) { echo social_id_group( $atts ); }
+
 
 }
 
